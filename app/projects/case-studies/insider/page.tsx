6a1c66fd77368d4482fbc3353e0519fe0fb36494
Rahul Singh <rahulkuulsingh@gmail.com>
2024-04-13 03:15:44 +0530

    did not track the changes as I was building this

---
+++ app/projects/case-studies/insider/page.tsx
@@ -0,0 +1,7 @@
+export default function Insider() {
+  return (
+    <>
+      <p>Insider</p>
+    </>
+  );
+}
---
+++ app/projects/case-studies/sakra/page.tsx
@@ -0,0 +1,7 @@
+export default function Sakra() {
+  return (
+    <>
+      <p>Sakra</p>
+    </>
+  );
+}
