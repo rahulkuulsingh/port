adf7dba8fc2f8b4eceefa7f6f73cd543235ba7cd
Rahul Singh <rahulkuulsingh@gmail.com>
2024-04-13 03:32:17 +0530

    escaped characters

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -7,7 +7,7 @@ export default function Projects() {
         <Card
           className="w-full"
           title="Enhancing Usability of Architect."
-          tags={["Web Dev", "React", "UI/UX"]}
+        //   tags={`"Web Dev", "React", "UI/UX"`}
           role="Product Designer, @Insider"
           projectDate="2021 - 2023"
           team="Sarper, Blazenko, Nour, Cansu, Barim"
