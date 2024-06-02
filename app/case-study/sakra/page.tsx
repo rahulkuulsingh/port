288bdd9b28dec9c47109f7cfd63da32d80501316
Rahul Singh <rahulkuulsingh@gmail.com>
2024-06-02 23:12:39 +0530

    sakra project

--- app/case-study/sakra/page.tsx
+++ app/case-study/sakra/page.tsx
@@ -9,10 +9,10 @@ import Video from "@/app/Components/Video";
 import StickyNotes from "@/app/Components/UI/Figjam/StikcyNotes";
 import HowMightWeSection from "@/app/Components/UI/Quote/HowMightWe";
 
-
 import DotGridBG from "@/app/Components/UI/CosmeticComponents/Backgrounds/DotGridBG";
 import TableOfContents from "@/app/Components/navigation/ContentStickyNav/TableOfContents";
 import SectionTitle from "@/app/Components/SectionTitle";
+import userFlow from "@/public/sakra/case-assets/useFlow.svg"
 
 import IntroductionGrid from "@/app/Components/IntroductionGrid";
 
@@ -184,73 +184,263 @@ export default function Insider() {
             subtitle="Research Phase"
             title="The app has diverse users, so we conducted user interviews to understand their needs."
           />
-          <p className="text-gray-400 mt-4">It is important to understand the different user groups who will be using the application. Listening to their stories will help us achieve this goal.</p>
+          <p className="text-gray-400 mt-4">
+            It is important to understand the different user groups who will be
+            using the application. Listening to their stories will help us
+            achieve this goal.
+          </p>
         </section>
         <section className="lg:max-w-[64rem] md:max-w-[48rem] mx-auto relative sm:max-w-[40rem] w-full">
-        <div className="relative max-w-[80rem] mx-auto mt-16 pt-4  ">
+          <div className="relative max-w-[80rem] mx-auto mt-16 pt-4  ">
+            <div className="grid grid-col-1 md:grid-cols-3 gap-4">
+              <div className="flex flex-col bg-zinc-900 gap-8  rounded-xl p-4 pb-8">
+                <div className="flex gap-4 items-center ">
+                  <img
+                    src="../sakra/case-assets/connect.svg"
+                    height={64}
+                    width={64}
+                    alt=""
+                  />
+                  <div className="flex flex-col">
+                    <p className="text-blue-600 font-semibold	text-lg">
+                      Ramesh Roy
+                    </p>
+                    <p>Manager at MNC</p>
+                    <p className="text-xs">Bengaluru, KA</p>
+                  </div>
+                </div>
+                <p className="mt-2 text-gray-400 text-sm">
+                  When I want to consult a doctor I want to know the timings as
+                  I am mostly busy on projects I work on.
+                </p>
+
+                <p className="mt-2 text-gray-400 text-sm">
+                  Some times, when a sudden meeting is organized and I am also
+                  part of the meeting I want to reschedule my appointments or
+                  may be cancel them.
+                </p>
+                <p className="mt-2 text-gray-400 text-sm">
+                  Going physically to a hospital or lab to collect lab reports
+                  takes time, I wish If I can access the reports on my mobile
+                  phone or laptop, without me physically going to collect it.
+                </p>
+              </div>
+              <div className="flex flex-col bg-zinc-900 gap-8  rounded-xl p-4 pb-8">
+                <div className="flex gap-4 items-center ">
+                  <img
+                    src="../sakra/case-assets/connect.svg"
+                    height={64}
+                    width={64}
+                    alt=""
+                  />
+                  <div className="flex flex-col">
+                    <p className="text-blue-600 font-semibold	text-lg">
+                      Deepti Jha
+                    </p>
+                    <p>Home Maker</p>
+                    <p className="text-xs">Indore, MP</p>
+                  </div>
+                </div>
+                <p className="mt-2 text-gray-400 text-sm">
+                  My younger son has kidney problems, he goes through dialysis
+                  three times every week. So it's important for me to know his
+                  health status.
+                </p>
+
+                <p className="mt-2 text-gray-400 text-sm">
+                  I want to closely monitor his health status, see reports, and
+                  want to consult with a doctor whenever we have an emergency.
+                </p>
+
+                <p className="mt-2 text-gray-400 text-sm">
+                  Making payments is a hassle for us. I want to make online
+                  payments, and want to keep track of payment history.
+                </p>
+              </div>
+              <div className="flex flex-col bg-zinc-900 gap-8  rounded-xl p-4 pb-8">
+                <div className="flex gap-4 items-center ">
+                  <img
+                    src="../sakra/case-assets/connect.svg"
+                    height={64}
+                    width={64}
+                    alt=""
+                  />
+                  <div className="flex flex-col">
+                    <p className="text-blue-600 font-semibold	text-lg">
+                      G. Maniraj
+                    </p>
+                    <p>Engineering student</p>
+                    <p className="text-xs">Anantapur, AP</p>
+                  </div>
+                </div>
+                <p className="mt-2 text-gray-400 text-sm">
+                  I stay in a hostel, but mom and dad stay at home. As I am not
+                  staying with them, it will be convenient for me to book
+                  appointments from my hostel.
+                </p>
+
+                <p className="mt-2 text-gray-400 text-sm">
+                  Dad is a diabetes patient, I want to see his reports and take
+                  actions accordingly.
+                </p>
+
+                <p className="mt-2 text-gray-400 text-sm">
+                  As dad wants to consult with the same doctor, I want to have a
+                  quick option to book an appointment with the same doctor he
+                  consulted with last time.
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="lg:max-w-[64rem] md:max-w-[48rem] md:px-8 mt-16 mx-auto pt-16 pb-8 px-6 relative sm:max-w-[40rem] w-full">
+          <SectionTitle
+            sectionID="survey"
+            subtitle="Reasearch Phase - Hypothesis Formation"
+            title="The following hypothesis can be formed by synthesizing the data obtained from the
+interview done earlier."
+          />
           <div className="grid grid-col-1 md:grid-cols-3 gap-4">
             <div className="flex flex-col bg-zinc-900 gap-8  rounded-xl p-4 pb-8">
-              <div className="flex gap-4 items-center ">
-                <img
-                  src="../sakra/case-assets/connect.svg"
-                  height={64}
-                  width={64}
-                  alt=""
-                />
-                <div className="flex flex-col">
-                  <p className="text-blue-600 font-semibold	text-lg">Ramesh Roy</p>
-                  <p>Manager at MNC</p>
-                  <p className="text-xs">Bengaluru, KA</p>
-                </div>
-              </div>
-              <p className="text-sm">
-                Find and book appointment with doctors easily by name,
-                specialty, or by symptoms
+              <p className="mt-2 text-gray-400 text-sm">
+                Patients will find it easier to search for doctors by symptoms
+                or specialty, but it's good to have the option to let them
+                search by doctor name as well.
+              </p>
+
+              <p className="mt-2 text-gray-400 text-sm">
+                Allowing the patient to visit the nearest facility will help
+                them save time. It will also reduce traffic in the main branch
+                of the hospital.
+              </p>
+
+              <p className="mt-2 text-gray-400 text-sm">
+                Patients or family members often want to know more about the
+                doctor, their experience, specialty, education, etc. It creates
+                trust and good relations between patients and doctors.
+              </p>
+
+              <p className="mt-2 text-gray-400 text-sm">
+                Keeping track of visit history and reminding patients of
+                upcoming appointments will be a nice idea, as sometimes users
+                forget about their appointments.
               </p>
             </div>
             <div className="flex flex-col bg-zinc-900 gap-8  rounded-xl p-4 pb-8">
-              <div className="flex gap-4 items-center ">
-                <img
-                  src="../sakra/case-assets/connect.svg"
-                  height={64}
-                  width={64}
-                  alt=""
-                />
-                <div className="flex flex-col">
-                  <p className="text-blue-600 font-semibold	text-lg">Deepti Jha</p>
-                  <p>Home Maker</p>
-                  <p className="text-xs">Indore, MP</p>
-                </div>
-              </div>
-              <p className="text-sm">
-                Find and book appointment with doctors easily by name,
-                specialty, or by symptoms
+              <p className="mt-2 text-gray-400 text-sm">
+                It will be a good idea to remind the patient to take medicine on
+                time as prescribed by the doctor or physician.
+              </p>
+              <p className="mt-2 text-gray-400 text-sm">
+                Allow patients to make appointments on their preferred date and
+                time. But if the doctor is not available on the selected date or
+                time, don't allow booking for the same.
+              </p>
+              <p className="mt-2 text-gray-400 text-sm">
+                Access to lab reports and case sheets will help patients know
+                more about their health. It will also save their time as they
+                won't have to physically collect it.
+              </p>
+              <p className="mt-2 text-gray-400 text-sm">
+                Giving options to book health checkups on a specific date can be
+                implemented, though users will have to visit the hospital or lab
+                physically if required.
               </p>
             </div>
             <div className="flex flex-col bg-zinc-900 gap-8  rounded-xl p-4 pb-8">
-              <div className="flex gap-4 items-center ">
-                <img
-                  src="../sakra/case-assets/connect.svg"
-                  height={64}
-                  width={64}
-                  alt=""
-                />
-                <div className="flex flex-col">
-                  <p className="text-blue-600 font-semibold	text-lg">G. Maniraj</p>
-                  <p>Engineering student</p>
-                  <p className="text-xs">Anantapur, AP</p>
-                </div>
-              </div>
-              <p className="text-sm">
-                Find and book appointment with doctors easily by name,
-                specialty, or by symptoms
+              <p className="mt-2 text-gray-400 text-sm">
+                Giving the option to reschedule appointments will greatly help
+                busy professionals.
+              </p>
+
+              <p className="mt-2 text-gray-400 text-sm">
+                Giving options to make and track payments online will not only
+                save time but also be easier and more convenient.
+              </p>
+
+              <p className="mt-2 text-gray-400 text-sm">
+                Some patients visit the same doctor multiple times in a week or
+                a month. Allowing them to save doctors as favorites will be a
+                time saver as they won't have to search again.
+              </p>
+
+              <p className="mt-2 text-gray-400 text-sm">
+                Some patients visit the same doctor multiple times in a week or
+                a month. Allowing them to save doctors as favorites will be a
+                time saver as they won't have to search again.
               </p>
             </div>
           </div>
-        </div>
         </section>
-        </div>
-
+        <section className="lg:max-w-[64rem] md:max-w-[48rem] md:px-8 mt-16 mx-auto pt-16 pb-8 px-6 relative sm:max-w-[40rem] w-full">
+          <SectionTitle
+            sectionID="survey"
+            subtitle="Reasearch Phase - Privacy and Security"
+            title="Health care data is very sensitive. Users want to know who has access to their data. Hence, privacy and security are top priorities while designing and developing these apps."
+          />
+          <div className="grid grid-col-1 md:grid-cols-3 gap-4">
+            <div className="flex flex-col bg-zinc-900 gap-8  rounded-xl p-4 pb-8">
+              <h3 className="font-semibold text-lg">
+                Two Factor Authentication
+              </h3>
+              <p className="mt-2 text-gray-400 text-sm">
+                Every time a user logs in, after entering the user credentials,
+                the user will have to enter the OTP sent to their registered
+                mobile number and email ID. If successful, the user will be
+                logged in.
+              </p>
+            </div>
+            <div className="flex flex-col bg-zinc-900 gap-8  rounded-xl p-4 pb-8">
+              <h3 className="font-semibold text-lg">
+                Allowing users to change their password.
+              </h3>
+              <p className="mt-2 text-gray-400 text-sm">
+                Passwords are painful if one forgets it. It&#39;s 8 characters
+                long, with at least one small letter, one capital letter, and
+                one special character. Users may forget their password or
+                someone may have gotten access to their account. To prevent
+                this, users can change their password and their registered
+                mobile number.
+              </p>
+            </div>
+            <div className="flex flex-col bg-zinc-900 gap-8  rounded-xl p-4 pb-8">
+              <h3 className="font-semibold text-lg">
+                Recovery and security questions.
+              </h3>
+              <p className="mt-2 text-gray-400 text-sm">
+                If the user forgets their password, they will have to answer
+                security questions. Then they will be prompted to enter the
+                registered mobile number and finally enter an OTP to
+                authenticate their identity. Once successfully done, they can
+                now change their password.
+              </p>
+            </div>
+            <div className="flex flex-col bg-zinc-900 gap-8  rounded-xl p-4 pb-8">
+              <h3 className="font-semibold text-lg">Limited access of data.</h3>
+              <p className="mt-2 text-gray-400 text-sm">
+                Patient data is only available to previously consulted doctors
+                and can be given to any other doctor if the patient grants
+                access to them.
+              </p>
+            </div>
+          </div>
+        </section>
+        <section className="relative max-w-[80rem] mx-auto mt-16 pt-16 w-full">
+          <SectionTitle
+            sectionID="survey"
+            subtitle="Designing - Information Architecture"
+            title="Based on user goals and business needs, I designed the user flow."
+          />
+          <div className="relative w-full ">
+            <Image
+              src={userFlow}
+              alt="Image description"
+              className="p-4 w-full"
+            />
+            <DotGridBG />
+          </div>
+        </section>
+      </div>
     </>
   );
 }
